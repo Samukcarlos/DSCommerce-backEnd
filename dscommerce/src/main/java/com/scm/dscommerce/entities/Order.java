578f92ba2090da735beb2efd29ba9ea737150d16
Samukcarlos <samukcarlos@gmail.com>
2023-03-12 13:14:01 -0300

    DsCommerce - Order

---
+++ dscommerce/src/main/java/com/scm/dscommerce/entities/Order.java
@@ -0,0 +1,59 @@
+package com.scm.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+@Entity // para que minha entiti possa ser mapeada em uma tabela no Banco de Dados H2
+@Table(name = "tb_order") // para personalizar o nome da tabelo no Banco de Dados H2
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")// para que seja salvo no banco de dados no padrão utc, depois convertemos para o horário local
+    private Instant moment;
+    private OrderStatus status;
+    @ManyToOne // mapeando muitos para um no banco de dados.
+    @JoinColumn(name = "client_id") // inclui um campo na tabela orde com o nome da chave estrangeira client_id (lado do "munitos")
+    private User client; // implementando relacionamento muitos para 1 (user Client)
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Order(Long id, Instant moment, OrderStatus status, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+
+    }
+}
+
---
+++ dscommerce/src/main/java/com/scm/dscommerce/entities/User.java
@@ -0,0 +1,99 @@
+package com.scm.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity // para que minha entiti possa ser mapeada em uma tabela no Banco de Dados H2
+@Table(name = "tb_user") // para personalizar o nome da tabelo no Banco de Dados H2
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto implementação no Banco de Dados relacional
+    private Long id;
+    private String nome;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+    @OneToMany(mappedBy = "client") // mapeamento muitos para um. inclui na tabela Order um campo na chave estrangeira
+    private List<Order> orders = new ArrayList<>(); // lado do "um para muintos" como são muitos precisa de uma lista.
+    // na classe User tenho uma lista de orders.
+    public User(){
+
+    }
+
+    public User(Long id, String nome, String email, String phone, LocalDate birthDate, String password) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Order> getOrders() { // Para acessar a lista
+        return orders;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", birthDate=" + birthDate +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
